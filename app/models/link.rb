a3470591947d5eaaf84422bbd7a77aedb744e4e8
Ignacio Senra Oka <isenras@hotmail.com>
2023-12-09 01:47:03 -0300

    Navbar changes

--- app/models/link.rb
+++
@@ -1,9 +0,0 @@
-class Link < ActiveRecord::Base
-    validates :slug, presence: true, uniqueness: true
-    validates :name, presence: true, uniqueness: true, allow_blank: true
-end
-       
-        
-
-
-
