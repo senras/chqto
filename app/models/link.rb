ebe913c3a004b8d6d3a59691b259a8cc2bfd4a29
Ignacio Senra Oka <isenras@hotmail.com>
2023-12-11 20:06:10 -0300

    Link model

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,13 +1,39 @@
 class Link < ApplicationRecord
-    validates :slug, presence: true
     belongs_to :user
-    validates :user, presence: true
+    has_many :visits, dependent: :destroy
+    validates :slug, presence: true, uniqueness: true
+    validates :name, presence: false
+    validates :link_type, presence: true
+    validates :expiration_date, presence: true , if: :is_temporary?
+    validates :password, presence: true , if: :is_private?
+    validates :url, presence: true ,format: { with: URI.regexp }
+    has_secure_password validations: false
+
+    enum link_type: { regular: 'regular', temporary: 'temporary', private: 'private', ephemeral: 'ephemeral' }, _prefix: true
+
+    before_validation :generate_slug_and_unique_url, on: :create
+    
+  # Types of links:
+  # - Links regulares
+  # - Links temporales - tiene atributo expiration_date datetime
+  # - Links privados - tiene atributo password string
+  # - Links efímeros - tiene atributo accessed boolean
+
+  def self.link_disable
+    update(accessed: true)
+  end  
+
+  private
+  def generate_slug_and_unique_url
+    self.slug ||= SecureRandom.hex(3)
+  end
+
+  def is_temporary?
+    link_type == 'temporary'
+  end
+
+  def is_private?
+    link_type == 'private'
+  end
+
 end
-
-# Types of links:
-# - Links regulares
-# - Links temporales - tienen ExpirationDate
-# - Links privados - tienen Password
-# - Links efímeros - tienen Accessed
-
-#$ rails generate scaffold Link name:string slug:string expiration_date:date password:string  accessed:boolean
