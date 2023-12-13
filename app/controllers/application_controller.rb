61c5afdc7d611c11ae64596abbb684558e3ea68f
Ignacio Senra Oka <isenras@hotmail.com>
2023-12-13 04:14:02 -0300

    Error handlers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
   
+    def not_found
+      # render file: "#{Rails.root}/app/views/errors/404.html", layout: false, status: :not_found
+      render file: Rails.root.join('public', '404.html'), status: :not_found
+    end
+
+    def forbidden
+      # render file: "#{Rails.root}/app/views/errors/403.html", layout: false, status: :forbidden
+      render file: Rails.root.join('public', '403.html'), status: :forbidden
+    end
+
     protected
   
     def configure_permitted_parameters
